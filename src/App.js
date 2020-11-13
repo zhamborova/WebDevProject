0973cc852cf0e0faf321e986322640bfa8824b74
Michelle Kang <kang.mic@husky.neu.edu>
2020-11-12 23:25:17 -0500

    added state for profile page, still need to change it to match updated profile UI

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import News from "./pages/news/news.js";
 import SingleEvent from "./pages/single-event/single-event";
 import CreateEvent from "./pages/create-event/create-event";
+import Profile from "./pages/profile/profile";
 import EventsSearch from "./pages/events-search/events-search";
 import FriendsSearch from "./pages/friends-search/friends-search";
 function App() {
@@ -16,7 +17,8 @@ function App() {
 
           <Route exact path={['/news', "/news/:title"]} component={News}/>
           <Route exact path={'/events/:eventId'} component={SingleEvent} />
-          <Route exact path={'/users/:userId/events'} component={CreateEvent} />
+          <Route exact path={'/users/:userId/events'} component={CreateEvent}/>
+          <Route exact path={'/users/:userId/profile'} component={Profile}/>
           <Route exact path={'/eventsSearch'} component={EventsSearch}/>
           <Route exact path={'/friendsSearch'} component={FriendsSearch}/>
       </Switch>
--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -2,48 +2,52 @@ import React from 'react';
 import './profile.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faLongArrowAltRight} from "@fortawesome/free-solid-svg-icons";
+import InputBox from "../../components/input-box/input-box";
 
+const profile = {
+    name: 'John Snow',
+    location: 'Boston',
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
+        " sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi " +
+        "ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " +
+        "in voluptate velit esse cillum dolore eu fugiat nulla.",
+    email: 'john.snow@gmail.com',
+    password: 'password'
+}
 
 class Profile extends React.Component {
 
+    state = {
+        profile: profile
+    }
+
     render() {
-        return(
+        return (
             <div className={"body"}>
                 <div className={"row"}>
                     <div className={"container profile-container col-8"}>
                         <h3>Your profile</h3>
-                        <div>
-                            <label>Name</label>
-                            <input className={"form-control"}/>
-                            <a>Done</a>
-                        </div>
-                        <div>
-                            <label>Location</label>
-                            <input className={"form-control"}/>
-                            <a>Done</a>
-
-                        </div>
-                        <div>
-                            <label>Email</label>
-                            <input className={"form-control"}/>
-                            <a>Done</a>
-
-                        </div>
-                        <div>
-                            <label>Password</label>
-                            <input className={"form-control"}/>
-                            <a>Done</a>
-
-                        </div>
+                        <InputBox field={this.state.profile.name} hasType={false}/>
+                        <InputBox field={this.state.profile.location} hasType={false}/>
+                        <InputBox field={this.state.profile.bio} hasType={false}/>
                     </div>
                     <div className={"col-4"}>
+                        <div className={"container right-column-container"}>
+                            <label>Email</label>
+                            <p>{this.state.profile.email}</p>
+                        </div>
+                        <div className={'container right-column-container'}>
+                            <label>Password</label>
+                            <p>{this.state.profile.password}</p>
+                        </div>
                         <span>
-                            <div className={"container faq-container"}>FAQ
+                            <div className={"container right-column-container"}>FAQ
                             <FontAwesomeIcon className="mt-1" icon={faLongArrowAltRight}/>
                             </div>
                         </span>
                         <span>
-                            <div className={"container report-container"}>Report a problem
+                            <div className={"container right-column-container"}>Report a problem
                             <FontAwesomeIcon className="mt-1" icon={faLongArrowAltRight}/>
                             </div>
                         </span>
